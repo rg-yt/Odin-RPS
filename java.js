2a59b1aa87ff5d144f62b2b3e09ed90be3fd3c23
Tyler <graytd1996@gmail.com>
2022-05-06 15:59:12 -0400

    Create function to generate a value between 0-2

--- java.js
+++ java.js
@@ -1,1 +1,19 @@
-console.log('Hello World!')
+
+
+// create function called computerPlay
+function computerPlay(){
+    // generate a number between 0-2
+    com = Math.floor(Math.random()*3);
+    // check value is working 
+    console.log(com);
+    // return rock, paper or scissors depeding on the value
+    if (com === 0){
+        return "Rock";
+    }
+    else if(com==1){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+}
