b37e36a23b0dbdc8072aa32be5d164fee77e902b
tygr <graytd1996@gmail.com>
2022-09-21 21:08:07 -0400

    Update playRound function to work with buttons

--- java.js
+++ java.js
@@ -1,61 +1,77 @@
-// create a computer choice
-// make computer choice random
-// get a choice from user 
-// compare computer and user choice
-// send result to user
 let wins = 0;
 let loss = 0;
 let score;
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', playRound);
+});
+const plscore = document.querySelector('.p-score');
+const cpuscore = document.querySelector('.c-score');
+const message = document.querySelector('.main');
+
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3);
     if (x == 0) {
-        return "rock";
+        return "Rock";
     } else if (x == 1) {
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     }
 }
+
 
 function playRound(playerSelection, computerSelection = getComputerChoice()) {
-    playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    while (!(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors")) {
-        playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    }
+    playerSelection = this.innerText;
     if (playerSelection === computerSelection) {
-        return `You picked ${playerSelection}. Your opponent picked ${computerSelection}. It's a Tie!`;
-    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
-        playerSelection === "paper" && computerSelection === "rock" ||
-        playerSelection === "scissors" && computerSelection === "paper") {
-        wins += 1;
-        return `You picked ${playerSelection}. Your opponent picked ${computerSelection}. You Win!`;
+        message.innerText = `You picked ${playerSelection}. Your opponent picked ${computerSelection}. It's a Tie!`;
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors" ||
+        playerSelection === "Paper" && computerSelection === "Rock" ||
+        playerSelection === "Scissors" && computerSelection === "Paper") {
+        wins +=1;
+        plscore.innerText = `${wins}`;
+        message.innerText = `You picked ${playerSelection}. Your opponent picked ${computerSelection}. You Win!`;
     } else {
-        loss += 1;
-        return `You picked ${playerSelection}. Your opponent picked ${computerSelection}. You Lost!`;
+        loss+=1;
+        cpuscore.innerText = `${loss}`;
+        message.innerText = `You picked ${playerSelection}. Your opponent picked ${computerSelection}. You Lost!`;
 
     }
-
+    if(wins == 5 || loss == 5){
+        resetScore;
+    }
+    
 }
+
 function resetScore() {
     score = wins;
     wins = 0;
     loss = 0;
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound());
-    }
-    if (wins > loss) {
-        resetScore();
-        return `You won ${score} rounds. You won the game!`
-    } else if (wins < loss) {
-        resetScore();
-        return `You won ${score} rounds. You lost the game!`
-    } else {
-        resetScore();
-        return `It's a tie game! Try again!`
-    }
 
-}
+
+
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(playRound());
+//     }
+//     if (wins > loss) {
+//         resetScore();
+//         return `You won ${score} rounds. You won the game!`
+//     } else if (wins < loss) {
+//         resetScore();
+//         return `You won ${score} rounds. You lost the game!`
+//     } else {
+//         resetScore();
+//         return `It's a tie game! Try again!`
+//     }
+
+// }
+
+// create a computer choice
+// make computer choice random
+// get a choice from user 
+// compare computer and user choice
+// send result to user
